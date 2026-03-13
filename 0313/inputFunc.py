6f7c6cf63cdd917002194ace86bdd297954d3e32
KKayoKKo <a74400793@gmail.com>
2026-03-13 11:42:00 +0900

    Add files via upload

---
+++ 0313/inputFunc.py
@@ -0,0 +1,7 @@
+NUM1 = int(input("첫 번째 숫자를 입력하세요:"))
+NUM2 = int(input("두 번째 숫자를 입력하세요:"))
+
+print(f"NUM1: {NUM1}, NUM2: {NUM2}, NUM3: {NUM1 + NUM2}")
+print(f"NUM1: {NUM1}, NUM2: {NUM2}, NUM3: {NUM1 * NUM2}")
+print(f"NUM1: {NUM1}, NUM2: {NUM2}, NUM3: {NUM1 - NUM2}")
+print(f"NUM1: {NUM1}, NUM2: {NUM2}, NUM3: {NUM1 / NUM2}")
