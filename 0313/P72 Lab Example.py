9e0d9c91c34d098c4c50551e1e31b67c47f2612a
KKayoKKo <a74400793@gmail.com>
2026-03-18 13:41:41 +0900

    Add files via upload

---
+++ 0313/P72 Lab Example.py
@@ -0,0 +1,5 @@
+speed = 300000.0 # 빛의 속도 300000km/sec
+disitance = 40000000000000.0 # 거리
+secs = disitance / speed # 걸리는 시간
+lightyear = secs / (60.0*60.0*24.0*365.0) # 단위를 광년으로 변환
+print(lightyear, "광년")
