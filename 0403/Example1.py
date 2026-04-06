8401f1f6f19309e61a73760fb3afd1511d78e5ed
KKayoKKo <a74400793@gmail.com>
2026-04-06 20:16:31 +0900

    Delete 0403 directory

--- 0403/Example1.py
+++
@@ -1,8 +0,0 @@
-def get_area(radius):
-    area = 3.14*radius**2 # 원의 면적 공식: π × r²
-    return area # 계산된 면적 값을 함수 밖으로 돌려줌.
-result = get_area(3)
-print("반지름이 3인 원의 면적=", result)
-
-# 반지름이 3인 원의 면적 구하는 예제.
-# 3.14×3^2 구하는 계산.
